0c9279e509fa69b239231ea38b63269830a14db0
Haley Boyle <haleyjboyle@gmail.com>
2018-12-28 14:33:17 -0700

    adding to github

---
+++ src/components/common/index.js
@@ -0,0 +1,6 @@
+export * from "./Button"; //imports and exports at same time
+export * from "./Card";
+export * from "./CardSection";
+export * from "./Header";
+export * from "./Input";
+export * from "./Spinner";
